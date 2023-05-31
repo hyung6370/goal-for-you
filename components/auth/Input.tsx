b3932d1ca98c23ae996cc3b5fc6d254cb8a8f109
emptyhead <korea6370@yu.ac.kr>
2023-05-31 21:19:16 +0900

    23/5/31 update

--- components/auth/Input.tsx
+++ components/auth/Input.tsx
@@ -1,6 +1,36 @@
-const Input = () => {
+import React from "react";
+
+interface InputProps {
+    id: string;
+    onChange: any;
+    value: string;
+    label: string;
+    type?: string;
+}
+
+const Input: React.FC<InputProps> = ({
+    id,
+    onChange,
+    value,
+    label,
+    type
+}) => {
     return (
-        <input />
+        <div className="relative">
+            <input
+                onChange={onChange}
+                type={type}
+                value={value}
+                id={id}
+                className="block w-full px-6 pt-6 pb-1 text-purple-700 bg-white border border-purple-700 rounded-3xl text-md apperance-none focus:outline-none focus:ring-0 peer"
+                placeholder=" "
+            />
+            <label
+                className="absolute duration-150 -translate-y-3 text-md text-zinc-400 transfomr scale-75 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3"
+                htmlFor={id}>
+                {label}
+            </label>
+        </div>
     )
 }
 
--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,18 +1,64 @@
 import Layout from "@/components/Home/Layout";
 import Input from "@/components/auth/Input";
+import { useCallback, useState } from "react";
 
 const Auth = () => {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [variant, setVariant] = useState('login');
+
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
+    }, []);
+
     return (
         <div className="relative h-full w-full bg-[url('/img/hero.jpg')]">
-            <div className="bg-purple-700 w-full h-full lg:bg-opacity-50">
+            <div className="w-full h-full bg-purple-700 lg:bg-opacity-50">
+                <nav className="px-12 py-5">
+                    <p className="text-4xl text-white font_here">
+                        Goal For you
+                    </p>
+                </nav>
                 <div className="flex justify-center">
-                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-                        <h2 className="text-white text-4xl mb-8 font-semibold">
-                            Sign in
+                    <div className="self-center w-full px-16 py-16 mt-2 bg-white bg-opacity-50 rounded-3xl lg:w-2/5 lg:max-w-md">
+                        <h2 className="flex justify-center pb-4 mb-8 text-5xl font-semibold text-purple-800 font_here">
+                            {variant === 'login' ? 'Sign in' : 'Register'}
                         </h2>
                         <div className="flex flex-col gap-4">
-                            <Input />
+                            {variant === 'register' && (
+                                <Input
+                                    label="Username"
+                                    onChange={(ev: any) => setName(ev.target.value)}
+                                    id="name"
+                                    value={name}
+                                />
+                            )}
+                            <Input
+                                label="Email"
+                                onChange={(ev: any) => setEmail(ev.target.value)}
+                                id="email"
+                                type="email"
+                                value={email}
+                            />
+                            <Input
+                                label="Password"
+                                onChange={(ev: any) => setPassword(ev.target.value)}
+                                id="password"
+                                type="password"
+                                value={password}
+                            />
                         </div>
+                        <button className="w-full py-3 mt-10 text-white transition bg-purple-700 rounded-3xl hover:bg-purple-500 ">
+                            {variant === 'login' ? 'Login' : 'Sign up'}
+                        </button>
+                        <p className="mt-12 text-neutral-700">
+                            {variant === 'login' ? 'First time using Goal for you?' : 'Already have an account?'}
+                            <span onClick={toggleVariant} className="ml-1 font-bold text-black cursor-pointer hover:underline">
+                                {variant === 'login' ? 'Create and account' : 'Login'}
+                            </span>
+                        </p>
                     </div>
                 </div>
             </div>
