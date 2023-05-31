cb9a07faa7f7c04a6ceac9b1dfd12c30308417de
emptyhead <korea6370@yu.ac.kr>
2023-05-31 23:30:56 +0900

    23/5/31 update

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -1,8 +1,15 @@
-import Layout from "@/components/Home/Layout";
 import Input from "@/components/auth/Input";
+import axios from "axios";
 import { useCallback, useState } from "react";
+import { signIn } from 'next-auth/react';
+import { useRouter } from "next/router";
+
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
 
 const Auth = () => {
+    const router = useRouter();
+    
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
@@ -12,6 +19,38 @@ const Auth = () => {
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
     }, []);
+
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+
+            router.push('/');
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }, [email, password, router]);
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password
+            });
+
+            login();
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }, [email, name, password, login]);
+
 
     return (
         <div className="relative h-full w-full bg-[url('/img/hero.jpg')]">
@@ -24,7 +63,7 @@ const Auth = () => {
                 <div className="flex justify-center">
                     <div className="self-center w-full px-16 py-16 mt-2 bg-white bg-opacity-50 rounded-3xl lg:w-2/5 lg:max-w-md">
                         <h2 className="flex justify-center pb-4 mb-8 text-5xl font-semibold text-purple-800 font_here">
-                            {variant === 'login' ? 'Sign in' : 'Register'}
+                            {variant === 'login' ? 'Login' : 'Register'}
                         </h2>
                         <div className="flex flex-col gap-4">
                             {variant === 'register' && (
@@ -50,13 +89,27 @@ const Auth = () => {
                                 value={password}
                             />
                         </div>
-                        <button className="w-full py-3 mt-10 text-white transition bg-purple-700 rounded-3xl hover:bg-purple-500 ">
+                        <button onClick={variant === 'login' ? login : register} className="w-full py-3 mt-10 text-white transition bg-purple-700 rounded-3xl hover:bg-purple-500 ">
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
+                        <div className="flex flex-row items-center justify-center gap-4 mt-8">
+                            <div
+                                onClick={() => signIn('google', { callbackUrl: '/' })}
+                                className="flex items-center justify-center w-10 h-10 transition bg-white rounded-full cursor-pointer hover:opacity-80"
+                            >
+                                <FcGoogle size={30} />
+                            </div>
+                            <div
+                                onClick={() => signIn('github', { callbackUrl: '/' })}
+                                className="flex items-center justify-center w-10 h-10 transition bg-white rounded-full cursor-pointer hover:opacity-80"
+                            >
+                                <FaGithub size={30} />
+                            </div>
+                        </div>
                         <p className="mt-12 text-neutral-700">
-                            {variant === 'login' ? 'First time using Goal for you?' : 'Already have an account?'}
+                            {variant === 'login' ? 'Goal for you를 처음 사용하시나요?' : '이미 계정을 가지고 계신가요?'}
                             <span onClick={toggleVariant} className="ml-1 font-bold text-black cursor-pointer hover:underline">
-                                {variant === 'login' ? 'Create and account' : 'Login'}
+                                {variant === 'login' ? '계정 생성 하러가기' : '로그인 하러가기'}
                             </span>
                         </p>
                     </div>
